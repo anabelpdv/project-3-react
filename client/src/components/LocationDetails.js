7f4a6f31365998f09ebce4b7b3ae79fe23423291
Anabel Perez <anabelpdv@gmail.com>
2019-11-06 15:32:25 -0500

    Update location details component

--- client/src/components/LocationDetails.js
+++ client/src/components/LocationDetails.js
@@ -12,24 +12,32 @@ export default class LocationDetails extends React.Component {
       <div>
           <h1>{location.title}</h1>
           <h4>{location.description}</h4>
+          <div className="gallery" id="gallery">
+            {location.imageUrl.map(image =>{
+              return(
+                  <div className="mb-3 pics animation all 2">
+                    <img className="img-fluid" src={image} alt="Card image cap"/>
+                  </div>
+                
+              )
+            })
+            }
+          </div> 
       </div>
       
     )
   }
 
   render() {
-    console.log(this.props.ready)
-  // console.log('Location details is rendered')
-  // console.log(this.props.visibleLocations)
-
+  //console.log('Location details is rendered')
     return (
-      <div>
+      <div className="details-background">
         {this.props.ready &&
                           
                           <div className="location-details-container">
                             <h1>Location details</h1>
                             {this.renderLocationDetails()}
-                          </div>              
+                            </div>               
                 }
       </div>
     )
